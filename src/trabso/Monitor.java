5b312bfa4921c3eef1002ee1bbd6d70636cb9906
mario.araujo <mario.araujo@reconcavo.org.com.br>
2015-08-31 14:22:25 -0300

    Alterações Método de adicionar página

--- src/trabso/Monitor.java
+++ src/trabso/Monitor.java
@@ -3,94 +3,114 @@ package trabso;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Monitor {
 
-    private PaginaMemoria pivot = null;
-    private List<PaginaMemoria> memoriaVirtual = null;
-    private Scanner sc = new Scanner(System.in);
+	private Integer idCount = 0;
+	private PaginaMemoria pivot = null;
+	private List<PaginaMemoria> memoriaVirtual = null;
+	private Scanner sc = new Scanner(System.in);
 
-    public void funcionar() {
-        Integer choice = construirMenu();
-        executarOperacaoEscolhida(choice);
+	public void funcionar() {
+		Integer choice;
+		do {
+			choice = construirMenu();
+			executarOperacaoEscolhida(choice);
+		} while (choice != 0);
 
-    }
+	}
 
-    private void executarOperacaoEscolhida(Integer choice) {
-        switch (choice) {
-            case 1: {
-                PaginaMemoria entrada = new PaginaMemoria();
-                
-                this.addPaginaMemoria(pivot, entrada);
-                break;
-            }
-            case 2: {
+	private void executarOperacaoEscolhida(Integer choice) {
+		try {
+			switch (choice) {
+				case 1: {
+					PaginaMemoria entrada = new PaginaMemoria();
+					entrada.setId(++idCount);
+					this.addPaginaMemoria(entrada);
+					break;
+				}
+				case 2: {
 
-                break;
-            }
-            case 3: {
+					break;
+				}
+				case 3: {
 
-                break;
-            }
-            case 4: {
+					break;
+				}
+				case 4: {
 
-                break;
-            }
-            default:
-                System.out.println("Opção inválida!");
-                break;
-        }
-    }
+					break;
+				}
+				case 0: {
+					System.out.println("Fim do programa!");
+					Thread.sleep(5000);
+					break;
+				}
+				default:
+					System.out.println("Opção inválida!");
+					break;
+			}
+		} catch (InterruptedException ex) {
+			Logger.getLogger(Monitor.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-    private Integer construirMenu() {
-        Integer choice = 0;
-        do {
-            System.out.println("Selecione uma opção abaixo!");
-            System.out.println("1) Adicionar Página de memória");
-            System.out.println("2) Remover Página de memória");
-            System.out.println("3) Visualizar todas as Páginas de memória");
-            System.out.println("4) Monitorar as Páginas de memória");
-            try {
-                choice = sc.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Formato inválido");
-            }
-            if (0 < choice && choice < 5) {
-                break;
-            }
-            System.out.print("\b");
-            System.out.flush();
-        } while (true);
-        return choice;
-    }
+	private Integer construirMenu() {
+		Integer choice = 0;
+		do {
+			System.out.println("Selecione uma opção abaixo!");
+			System.out.println("1) Adicionar Página de memória");
+			System.out.println("2) Remover Página de memória");
+			System.out.println("3) Visualizar todas as Páginas de memória");
+			System.out.println("4) Monitorar as Páginas de memória");
+			try {
+				choice = sc.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Formato inválido");
+			}
+			if (0 <= choice && choice < 5) {
+				break;
+			}
+			System.out.print("\b");
+			System.out.flush();
+		} while (true);
+		return choice;
+	}
 
-    private void addPaginaMemoria(PaginaMemoria atual, PaginaMemoria nova) {
-        if (atual == null) {
-            atual = nova;
-            atual.setProximo(atual);
-        } else {
-            if (atual.getProximo().equals(pivot)) {
-                atual.setProximo(nova);
-                nova.setProximo(pivot);
-            } else {
-                addPaginaMemoria(nova, atual.getProximo());
-            }
-        }
-    }
+	private void addPaginaMemoria(PaginaMemoria nova) {
+		if (pivot == null) {
+			pivot = nova;
+			pivot.setProximo(pivot);
+		} else {
+			addRecursivo(pivot, nova);
+		}
+	}
 
-    private void removePaginaMemoria(PaginaMemoria anterior, PaginaMemoria excluida) {
-        if (anterior == null) {
-            System.out.println("Lista vazia!");
-        } else {
-            if (anterior.getId().equals(excluida.getId())) {
-                anterior = anterior.getProximo();
-            } else if (anterior.getProximo().getId().equals(excluida.getId())) {
-                anterior.setProximo(anterior.getProximo().getProximo());
-                excluida.setProximo(null);
-            } else {
-                removePaginaMemoria(anterior.getProximo(), excluida);
-            }
-        }
-    }
+	private void addRecursivo(PaginaMemoria atual, PaginaMemoria nova) {
+		if (atual.getProximo().equals(atual)) {
+			atual.setProximo(nova);
+			nova.setProximo(atual);
+		} else {
+			addRecursivo(atual, nova);
+		}
+
+	}
+
+	private void removePaginaMemoria(PaginaMemoria anterior, PaginaMemoria excluida) {
+		if (anterior == null) {
+			System.out.println("Lista vazia!");
+		} else {
+			if (anterior.getId().equals(excluida.getId())) {
+				anterior = anterior.getProximo();
+			} else if (anterior.getProximo().getId().equals(excluida.getId())) {
+				anterior.setProximo(anterior.getProximo().getProximo());
+				excluida.setProximo(null);
+			} else {
+				removePaginaMemoria(anterior.getProximo(), excluida);
+			}
+		}
+	}
 
 }
--- src/trabso/PaginaMemoria.java
+++ src/trabso/PaginaMemoria.java
@@ -1,38 +1,62 @@
 package trabso;
 
+import java.util.Objects;
+
 public class PaginaMemoria {
 
-    private Integer id;
-    private Integer contAcesso;
-    private PaginaMemoria proximo;
+	private Integer id;
+	private Integer contAcesso;
+	private PaginaMemoria proximo;
 
-    public PaginaMemoria() {
-        contAcesso = 0;
-        proximo = null;
-    }
+	public PaginaMemoria() {
+		contAcesso = 0;
+		proximo = null;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public Integer getContAcesso() {
-        return contAcesso;
-    }
+	public Integer getContAcesso() {
+		return contAcesso;
+	}
 
-    public void setContAcesso(Integer contAcesso) {
-        this.contAcesso = contAcesso;
-    }
+	public void setContAcesso(Integer contAcesso) {
+		this.contAcesso = contAcesso;
+	}
 
-    public PaginaMemoria getProximo() {
-        return proximo;
-    }
+	public PaginaMemoria getProximo() {
+		return proximo;
+	}
 
-    public void setProximo(PaginaMemoria proximo) {
-        this.proximo = proximo;
-    }
+	public void setProximo(PaginaMemoria proximo) {
+		this.proximo = proximo;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 3;
+		hash = 23 * hash + Objects.hashCode(this.id);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final PaginaMemoria other = (PaginaMemoria) obj;
+		if (!Objects.equals(this.id, other.id)) {
+			return false;
+		}
+		return true;
+	}
 
 }
